265cd9ff14ae3217cfc8da9c5726621e8d04b97e
Manoj Kokkula <kokkulamanoj403@gmail.com>
2024-07-15 04:48:27 +0000

    Day_1 tasks

---
+++ Day_1.js
@@ -0,0 +1,12 @@
+//variable declaration
+//declare a variable var, assign it a number , and log the value to the console
+var a = 60;
+console.log(a);
+//declare a variable using let,assign it a. string,and log the value to the console
+let str = "manoj";
+console.log(str);
+
+//constant declaration
+ const a = 19;
+ 
+
